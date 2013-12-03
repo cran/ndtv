1c3e4ccada456284cbd38602334001ecf315947b
Skye Bender-deMoll <skyebend@uw.edu>
2013-12-03 00:00:00 +0000

    version 0.4.0

--- tests/networkAnimationTest.R
+++ tests/networkAnimationTest.R
@@ -7,141 +7,10 @@
 #
 #  Copyright 2003-2012 Statnet Commons
 #######################################################################
-#test some functions for calculating animated layouts and exporting movies.
+#test some auxillury functions for calculating  layouts etc.
 require(ndtv)
 require(testthat)
 
-# hard to write tests since most require a human looking at the output
-# most overall tests are included as man examples or in the packae vignette
-# this file should be mostly unit tests for functionality
-
-
-# ----- compute.animation ------
-
-# does it crash on basic example
-data(newcomb)
-dynNew <- networkDynamic(network.list=newcomb)
-dyn<-dynNew
-slice.par<-list(start=0,end=5,interval=1, aggregate.dur=1,rule=NULL)
-set.seed(123)
-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-
-
-# modify network argument in place?
-if(!all(c("animation.x.active","animation.y.active")%in%list.vertex.attributes(dyn))){
-  stop("compute.animation appears to have not correctly modified its argument in place")
-}
-
-# does it store slice.par
-par<-get.network.attribute(dyn,'slice.par')
-if (!all(names(par)==c("start","end","interval","aggregate.dur","rule" ))){
-  stop("compute.animation did not store slice parameters correctly in slice.par")
-}
-
-# does it store animation.x and animation.y
-x<-get.vertex.attribute(dyn,"animation.x.active")
-y<-get.vertex.attribute(dyn,"animation.y.active")
-if(any(is.na(x)) | any(is.na(y))){
-  stop('compute.animation did not store animation.x and animation.y vertex attributes')
-}
-
-# does it give same results from same seed
-x1 <-get.vertex.attribute.active(dyn,"animation.x",at=1)
-set.seed(123)
-dyn <-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-x2 <-get.vertex.attribute.active(dyn,"animation.x",at=1)
-if (!all(x1==x2)){
-  stop("compute.animation did not generate the same results fromt he same seed")
-}
-
-# does aggregate.dur change results
-set.seed(123)
-slice.par<-list(start=0,end=2,interval=1, aggregate.dur=1,rule="any")
-dyn <-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-x1 <-get.vertex.attribute.active(dyn,"animation.x",onset=1,terminus=2)
-slice.par<-list(start=0,end=2,interval=1, aggregate.dur=2,rule="any")
-set.seed(123)
-dyn <-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-x2 <-get.vertex.attribute.active(dyn,"animation.x",onset=1,terminus=2)
-if (any(x1==x2)){
-  stop("compute.animation did not give different results with a different aggreagate.dur")
-}
-
-# does aggregation rule change results
-set.seed(123)
-slice.par<-list(start=0,end=2,interval=1, aggregate.dur=2,rule="any")
-dyn <-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-x1 <-get.vertex.attribute.active(dyn,"animation.x",at=1)
-slice.par<-list(start=0,end=2,interval=1, aggregate.dur=2,rule="all")
-set.seed(123)
-dyn <-compute.animation(dyn,slice.par=slice.par,verbose=FALSE)
-x2 <-get.vertex.attribute.active(dyn,"animation.x",at=1)
-if (any(x1==x2)){
-  stop("compute.animation did not give different results with a different aggregation rule")
-}
-
-# does default.dist change results
-data(stergm.sim.1)
-set.seed(123)
-slice.par<-list(start=0,end=2,interval=1, aggregate.dur=1)
-stergm.sim.1 <-compute.animation(stergm.sim.1,slice.par=slice.par,verbose=FALSE)
-x1 <-get.vertex.attribute.active(stergm.sim.1,"animation.x",at=1)
-set.seed(123)
-stergm.sim.1 <-compute.animation(stergm.sim.1,slice.par=slice.par,default.dist=100,verbose=FALSE)
-x2 <-get.vertex.attribute.active(stergm.sim.1,"animation.x",at=1)
-if (any(x1==x2)){
-  stop("compute.animation did not give different results with a different default.dist")
-}
-
-
-# check with incomplete slice.par
-expect_error(compute.animation(stergm.sim.1,slice.par=list()),"the 'slice.par' argument to compute.animation must include")
-expect_error(compute.animation(stergm.sim.1,slice.par=list(start=1)),"the 'slice.par' argument to compute.animation must include")
-expect_error(compute.animation(stergm.sim.1,slice.par=list(start=1,end=5,interval=1)),"the 'slice.par' argument to compute.animation must include")
-
-#----- render animation ------
-# does it crash on basic example
-# shorten time range so wont dake as long
-dyn<-network.extract(dynNew,onset=0,terminus=3,trim.spells=TRUE)
-render.animation(dyn,verbose=FALSE)
-
-# does replay work 
-ani.replay()
-
-# does increasing tween produce different results
-
-# does show.time crash if changed from default
-render.par<-list(tween.frames=2,show.time=FALSE)
-render.animation(dyn,render.par=render.par,verbose=FALSE)
-
-# does show.stats crashed if changed from default
-render.par<-list(tween.frames=2,show.time=TRUE,show.stats=FALSE)
-render.animation(dyn,render.par=render.par,verbose=FALSE)
-
-# can it render arbitrary plotting comands?
-render.par=list(tween.frames=2,show.time=TRUE,show.stats=NULL,extraPlotCmds=expression(text(0,0,"SOME TEXT ON THE PLOT",col='blue')))
-render.animation(dyn,render.par=render.par,verbose=FALSE)
-
-# test workaround for 0-coord label bug #322
-test<-network.initialize(2)
-activate.vertex.attribute(test,'x',0,onset=0,terminus=2)
-activate.vertex.attribute(test,'y',0,onset=0,terminus=2)
-compute.animation(test, animation.mode='useAttribute',layout.par = list(x = "x", y = "y"),slice.par=list(start=0,end=1,interval=1,aggregate.dur=0))
-render.animation(test,displaylabels=TRUE)
-
-
-# single vertex matrix collapse bug #332
-compute.animation(as.networkDynamic(network.initialize(1)))
-
-# test animation of single vertex
-test<-network.initialize(3)
-deactivate.vertices(test)
-activate.vertices(test,v=2,onset=0,terminus=3)
-compute.animation(test)
-render.animation(test)
-
-# test specifying xlim and ylim
-render.animation(test,xlim=c(-1,1),ylim=c(-1,1))
 
 # ----- java tests ----
 
@@ -167,11 +36,26 @@ if (!all(symat == matrix(c(0,1,2,20, 1,0,1,20, 2,1,0,20, 20,20,20,0),ncol=4,byro
   stop("layout.distance did not return symetric geodedesic distance with default.dist as expected")
 }
 
+
+# test using a function to specifiy a graphic plotting property
+test<-network.initialize(5)
+add.edges.active(test,onset=0,terminus=10,tail=1:4,head=2:5)
+activate.vertices(test,v=5,onset=4,terminus=10)
+activate.vertex.attribute(test,'status',0,onset=0,terminus=10)
+activate.vertex.attribute(test,'status',1,onset=1,terminus=10,v=1)
+activate.vertex.attribute(test,'status',1,onset=2,terminus=10,v=2)
+activate.vertex.attribute(test,'status',1,onset=3,terminus=10,v=3)
+activate.vertex.attribute(test,'status',1,onset=4,terminus=10,v=4)
+compute.animation(test)
+render.animation(test,vertex.col=function(slice){ifelse(slice%v%'status'==1,'red','blue')})
+
 # ----- kk layout ------
 
 # ----- mdsj layout -----
 # ndtv:::install.mdsj()
 
+# test a bad function arg
+expect_error(render.animation(test,vertex.col=function(peaches){ifelse(peaches%v%'status'==1,'red','blue')}),'unknown argument name')
 
 
 
